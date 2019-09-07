83097d4052750aac42d5f9805d47f8371a3b4185
tejaskadam <tejaskadam77@gmail.com>
2019-09-07 13:39:53 +0530

    synchronization

--- MultiThreading/src/com/example/threadCreation/SynchronisationDemo.java
+++ MultiThreading/src/com/example/threadCreation/SynchronisationDemo.java
@@ -40,14 +40,7 @@ public class SynchronisationDemo {
 		Demo b=new Demo(s,"patil");
 		a.start();
 		b.start();
-//		for (int i = 0; i < 10; i++) {
-//			try {
-//				Thread.sleep(1000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//			System.out.println("in main thread");
-//		}
+
 	}
 
 }
